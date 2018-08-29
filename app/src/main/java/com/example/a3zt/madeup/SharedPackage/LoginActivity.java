2a82d053499d4ecf1cbe9dfc0d3fdb6071f6e9a5
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2018-08-29 16:59:56 +0200

    29_Aug_A

--- app/src/main/java/com/example/a3zt/madeup/SharedPackage/LoginActivity.java
+++ app/src/main/java/com/example/a3zt/madeup/SharedPackage/LoginActivity.java
@@ -1,11 +1,13 @@
-package com.example.a3zt.madeup;
+package com.example.a3zt.madeup.SharedPackage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a3zt.madeup.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_shared_login);
     }
 }

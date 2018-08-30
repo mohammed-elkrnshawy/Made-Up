724f25242360f031bed8d6c809cc0d79721b4639
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2018-08-30 22:43:22 +0200

    30_Aug_A

--- app/src/main/java/com/example/a3zt/madeup/SharedPackage/SplashActivity.java
+++ app/src/main/java/com/example/a3zt/madeup/SharedPackage/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.a3zt.madeup.SharedPackage;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -17,5 +19,21 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_shared_splash);
+
+
+        new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                Intent mainActivity = new Intent(SplashActivity.this, StartActivity.class);
+                startActivity(mainActivity);
+                finish();
+            }
+        }.start();
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/a3zt/madeup/SharedPackage/StartActivity.java
@@ -0,0 +1,15 @@
+package com.example.a3zt.madeup.SharedPackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.a3zt.madeup.R;
+
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strart);
+    }
+}

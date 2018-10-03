865de55d37cfe5c371d469fa51a42edf8223df6d
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2018-10-03 10:28:40 +0200

    3_October2018_A

--- app/src/main/java/com/example/a3zt/madeup/HandMaker/HomeFragment/ProfileFragment.java
+++ app/src/main/java/com/example/a3zt/madeup/HandMaker/HomeFragment/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.a3zt.madeup.HandMaker.HomeFragment;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,13 +9,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.a3zt.madeup.R;
 import com.example.a3zt.madeup.SharedPackage.Class.ResponseUsers.DataUser;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 @SuppressLint("ValidFragment")
 public class ProfileFragment extends Fragment {
 
+    private CircleImageView image_Profile;
+    private TextView textName;
     private DataUser user;
     private View view;
 
@@ -28,8 +37,41 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view= inflater.inflate(R.layout.fragment_seller_profile , container , false) ;
 
+        InitComponent();
 
+
+        showData();
 
         return view;
+    }
+
+    private void showData() {
+        ImageLoader.getInstance().displayImage(user.getPhone(), image_Profile, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+
+            }
+        });
+        textName.setText(user.getName());
+    }
+
+    private void InitComponent() {
+        image_Profile=view.findViewById(R.id.image_profile);
+        textName=view.findViewById(R.id.txtName);
     }
 }
